747cae6821936dfb1686aecd547521089ef345d8
ovanv <vanvureno@cardiff.ac.uk>
2025-04-22 18:23:24 +0100

    Prepped

--- prepare_for_online.sh
+++ prepare_for_online.sh
@@ -3,8 +3,8 @@
 #rm -rf build/html/sources build/html/static
 #mv build/html/_sources build/html/sources
 #mv build/html/_static build/html/static
-#sed -e "s/_static/static/g" -e "s/_sources/sources/g" build/html/index.html > temp
-#mv temp build/html/index.html
+sed -e "s/_static/static/g" -e "s/_sources/sources/g" build/html/index.html > temp
+mv temp build/html/index.html
 
 ln -s build/html/index.html home
 ln -s build/html/services.html services
