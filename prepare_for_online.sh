9cba84f1b1e7abc23a7a2e7ed7b464b687ae0a3b
ovanv <vanvureno@cardiff.ac.uk>
2025-04-22 18:25:52 +0100

    Rebuilt and moved index

--- prepare_for_online.sh
+++ prepare_for_online.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#rm -rf build/html/sources build/html/static
-#mv build/html/_sources build/html/sources
-#mv build/html/_static build/html/static
+rm -rf build/html/sources build/html/static
+mv build/html/_sources build/html/sources
+mv build/html/_static build/html/static
 sed -e "s/_static/static/g" -e "s/_sources/sources/g" build/html/index.html > temp
 mv temp build/html/index.html
 
-ln -s build/html/index.html home
-ln -s build/html/services.html services
-ln -s build/html/projects.html projects
-ln -s build/html/blogs.html blogs
+#ln -s build/html/index.html home
+#ln -s build/html/services.html services
+#ln -s build/html/projects.html projects
+#ln -s build/html/blogs.html blogs
